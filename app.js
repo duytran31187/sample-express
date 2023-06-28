105b9e8a5c7dd8ced3b97ad065ee709a20b95cfd
Duy Tran <duy.tranquoc@gforces.auto>
2023-06-28 21:24:06 +0700

    serve static files

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); // Serving static files: ex: http://localhost:3000/images/1.png
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
